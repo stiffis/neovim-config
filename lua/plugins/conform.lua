919cba477ee94e954c3b6bc8044953e98c8dc67b
stiffis <steve.ildefonso@utec.edu.pe>
2026-09-21 17:56:41 -0500

    feat(conform): prettier for jsx/tsx/css/scss/html, use lsp_format
    lsp_fallback is deprecated in favor of lsp_format = "fallback".

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -11,14 +11,19 @@ return {
       python = { "isort", "black" },
       javascript = { "prettier" },
       typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      css = { "prettier" },
+      scss = { "prettier" },
+      html = { "prettier" },
       json = { "prettier" },
       yaml = { "prettier" },
       markdown = { "prettier" },
       typst = { "typstyle" },
     },
     format_on_save = {
-      timeout_ms = 3000, -- 1 segundo de tiempo límite (latexindent puede ser lento a veces)
-      lsp_fallback = true,
+      timeout_ms = 3000, -- 3 segundos: latexindent puede ser lento
+      lsp_format = "fallback",
     },
   },
 }
